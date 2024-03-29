21c5930910c093019805481f233d4bec79073592
Zerargui <hakim.zerargui@hotmail.com>
2024-03-29 15:28:40 +0100

    Mise en place de SpringSecurity, FeignConfig  et Dockerfile MongoDB

--- src/main/java/com/medilabosolutions/PatientNoteService/PatientNoteServiceApplication.java
+++ src/main/java/com/medilabosolutions/PatientNoteService/PatientNoteServiceApplication.java
@@ -1,7 +1,10 @@
 package com.medilabosolutions.PatientNoteService;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
 
 @SpringBootApplication
 public class PatientNoteServiceApplication {
